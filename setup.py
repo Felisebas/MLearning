91cb2e1ac3159ee34780c7bd1c45860d9e8eeb10
Felisebas <115413505+Felisebas@users.noreply.github.com>
2023-10-31 16:24:05 +0100

    commit

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  4 15:54:58 2022
+A brief comment just to modify the file
+@author: Felipe
+"""
+
 from setuptools import setup, find_packages
-from MLearning import _author,version,name_
+from MLearning import __author__,__version__,__name__
 
 
-VERSION = _version_
-AUTHOR = _author_
-NAME = _name_
+VERSION = __version__
+AUTHOR = __author__
+NAME = __name__
 
 setup(
     name                    = NAME,
@@ -16,11 +23,10 @@ setup(
     python_requires         = '>=3.9.5',
     packages                = find_packages(),
     include_package_data    = True,
-    package_data            = {'': ['resources/.csv','resources/clusters/.csv']},
+    package_data            = {'': ['resources/*.csv','resources/clusters/*.csv']},
     install_requires        = [
                                 'pandas',
                                 'numpy',
                                 'torch'
                                 ]
      )
-                                
